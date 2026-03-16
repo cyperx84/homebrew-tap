5feb1b5a158bd05ec80f4ba252d4224ab520d0b7
cyperx <cyperx84@gmail.com>
2026-03-16 14:10:02 +1000

    chore: clawforge v3.0.0 Go binary formula

--- Formula/clawforge.rb
+++ Formula/clawforge.rb
@@ -1,27 +1,23 @@
 class Clawforge < Formula
   desc "Forge and manage fleets of OpenClaw agents"
   homepage "https://github.com/cyperx84/clawforge"
-  url "https://github.com/cyperx84/clawforge/archive/refs/tags/v2.0.1.tar.gz"
-  sha256 "9568edc52e32c7afb648fb9f63c548d97aa829cd371099feb680388668cd85e8"
+  url "https://github.com/cyperx84/clawforge/archive/refs/tags/v3.0.0.tar.gz"
+  sha256 "bf80b71dd06f4d8e9c877be7c93d87d1f163c5b33f51df44c93719dda6399189"
   license "MIT"
   head "https://github.com/cyperx84/clawforge.git", branch: "main"
 
-  depends_on "bash"
-  depends_on "git"
-  depends_on "gh"
-  depends_on "jq"
-  depends_on "tmux"
+  depends_on "go" => :build
 
   def install
-    libexec.install Dir["*"]
-    bin.install_symlink libexec/"bin/clawforge" => "clawforge"
-    if File.exist?("bin/clawforge-dashboard")
-      bin.install "bin/clawforge-dashboard"
-    end
+    ldflags = %W[
+      -s -w
+      -X github.com/cyperx84/clawforge/pkg/version.Version=#{version}
+    ]
+    system "go", "build", *std_go_args(ldflags: ldflags), "."
   end
 
   test do
-    assert_match "clawforge v", shell_output("#{bin}/clawforge version")
+    assert_match version.to_s, shell_output("#{bin}/clawforge version")
     assert_match "Usage:", shell_output("#{bin}/clawforge help")
   end
 end
