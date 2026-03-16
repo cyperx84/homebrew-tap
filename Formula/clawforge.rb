39bb4658f4affc9bcfe3a2c8e86862d634ebdaae
cyperx <cyperx84@gmail.com>
2026-03-16 19:50:14 +1000

    chore: clawforge v3.2.0

--- Formula/clawforge.rb
+++ Formula/clawforge.rb
@@ -1,8 +1,8 @@
 class Clawforge < Formula
   desc "Forge and manage fleets of OpenClaw agents"
   homepage "https://github.com/cyperx84/clawforge"
-  url "https://github.com/cyperx84/clawforge/archive/refs/tags/v3.1.1.tar.gz"
-  sha256 "8f1d0a0d83e60430dfddfe94ebbc267bc99de65159c1851dc8c466f79279ae9d"
+  url "https://github.com/cyperx84/clawforge/archive/refs/tags/v3.2.0.tar.gz"
+  sha256 "fa728e32821da716499156ec50fafb6948138414739046e5df91d8db8a29e70a"
   license "MIT"
   head "https://github.com/cyperx84/clawforge.git", branch: "main"
 
